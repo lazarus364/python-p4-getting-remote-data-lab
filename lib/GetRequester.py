ea22d48516b3903e3be660de22150d56393f6f0c
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2025-02-14 10:15:05 +0300

    flask

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body.decode('utf-8'))
+
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)
+    print(requester.load_json())
